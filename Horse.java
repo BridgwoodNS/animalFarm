30a1f7ba5df0e97d4e8dd77a249f2d0f81dcb65e
Ryan Bridgwood <rybridgwood@gmail.com>
2022-09-01 18:16:52 -0400

    Added Horse

---
+++ Horse.java
@@ -0,0 +1,13 @@
+public class Horse {
+
+      public static void main(String[] args) {
+        Horse horse = new Horse();
+        horse.speak();
+    }
+
+    void speak(){
+
+        System.out.println("NAHHHHHH");
+    }
+
+}
